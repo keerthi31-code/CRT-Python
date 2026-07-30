dcd937897ec3f64f44dfb53bed44ab16844647d5
keerthi <muchhakeerthi@gmail.com>
2026-07-30 12:24:24 +0530

    LinkedList

--- linkedlist.py
+++ linkedlist.py
@@ -25,6 +25,16 @@ class node:
 class SLL:
     def __init__(self):
         self.head=None
+    def insert_at_begin(self, data):
+        new=node(data)
+        new.next=self.head
+        self.head=new
+    def insert_at_end(self, data):
+        new=node(data)
+        temp=self.head
+        while temp.next is not None:
+            temp=temp.next
+        temp.next=new      
     def Traversal(self):
         if self.head is None:
             print("linked list is empty.")
@@ -33,6 +43,7 @@ class SLL:
             while temp:
                 print(temp.data, end="-> ")
                 temp=temp.next
+new=node(5)
 n1=node(10)
 n2=node(20)
 n3=node(30)
@@ -44,5 +55,8 @@ n1.next=n2
 n2.next=n3
 n3.next=n4
 n4.next=n5
+sll.insert_at_begin(5)
+sll.insert_at_end(60)
+
 sll.Traversal()
 
