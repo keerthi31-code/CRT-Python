d20d8c93d10719de71f4e04cd5e3906e1e72a247
keerthi <muchhakeerthi@gmail.com>
2026-08-05 10:49:45 +0530

    BST

--- BST.py
+++ BST.py
@@ -10,16 +10,16 @@ def inorder(root):
         inorder(root.right)
 def preorder(root):
     if root:
-        preorder(root.left)
         print(root.data)
+        preorder(root.left)
         preorder(root.right)
         
 
 def postorder(root):
     if root:
         postorder(root.left)
-        print(root.data)
         postorder(root.right)
+        print(root.data)
 #searching
 def search(root,key):
     if root is None:
@@ -31,19 +31,60 @@ def search(root,key):
     else:
         return search(root.right,key)
 #insertion
-    def insert():
+def insert(root,key):
+    if root is None:
+        return Node(key)
+    if key<root.data:
+        root.left=insert(root.left,key)
+    elif key>root.data:
+        root.right=insert(root.right,key)
+    return root
+def deletion(root,key):
+    if root is None:
+        return None
+    if key<root.data:
+        root.left=deletion(root.left,key)
+    elif key>root.data:
+        root.right=deletion(root.right,key)
+    else:
+        if root.left is None and root.right is None:
+            return None
+    return root
+def delete_one(root,key):
+    if root is None:
+        return None
+    if key<root.data:
+        root.left=delete_one(root.left, key)
+    elif key>root.data:
+        root.right=delete_one(root.right,key)
+    else:
+        if root.right is None:
+            return root.left
+        else:
+            return root.right
+    return root
 
-root = Node(10)
-root.left = Node(20)
+root = Node(20)
+root.left = Node(10)
 root.right = Node(30)
-root.left.left = Node(40)
-root.left.right = Node(50)
-print("Postorder Traversal:")
-postorder(root)
-print("preorder:")
-preorder(root)
-key=80
-print(search(root,key))
+root.left.left = Node(5)
+root.left.right = Node(15)
 
+print("Inorder:")
+inorder(root)
 
+print("Search:", search(root, 15))
 
+root = insert(root, 25)
+
+print("After Insert:")
+inorder(root)
+
+root = deletion(root, 10)
+
+print("After Delete:")
+inorder(root)
+
+root=delete_one(root, 15)
+print("after deletion one node:")
+inorder(root)
