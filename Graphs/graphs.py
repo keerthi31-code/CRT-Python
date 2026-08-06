27d95e16520e82297b958957120117cae5ad382a
keerthi <muchhakeerthi@gmail.com>
2026-08-06 15:48:32 +0530

    Graphs

--- Graphs/graphs.py
+++ Graphs/graphs.py
@@ -11,24 +11,76 @@ for m in graph:
     print(m)
 
 #dict 
-graph={}
-def add_edge(u,v):
-    if u not in graph:
-        graph[u]=[]
-    if v not in graph:
-        graph[v]=[]
-    graph[u].append(v)
-    graph[v].append(u)
-add_edge('A','B')
+# graph={}
+# def add_edge(u,v):
+#     if u not in graph:
+#         graph[u]=[]
+#     if v not in graph:
+#         graph[v]=[]
+#     graph[u].append(v)
+#     graph[v].append(u)
+# add_edge('A','B')
 
 # Traversing 
 #BFS -- Breadth first Search
 '''
          A
         / \        |A|B|C|D|E|F|
-        B  C
-       / \  \
-       D  E  F
+        B  C        it follows the queue 
+       / \  \       popped element add to the list
+       D  E  F      
 '''
+from collections import deque
+def bfs(graph, start):
+    visited=set()
+    queue=deque([start])
+    visited.add(start)
+    while queue:
+        vertex=queue.popleft()
+        print(vertex, end=" ")
+        for neighbour in graph[vertex]:
+            if neighbour not in visited:
+                visited.add(vertex)
+                queue.append(neighbour)
+graph={
+    'A':['B','C'],
+    'B':['A','D','E'],
+    'C':['A','F'],
+    'D':['B'],
+    'E':['B'],
+    'F':['C']
+}
+
+bfs(graph,'A')
 
 
+#DFS
+
+
+def dfs(graph,start):
+    visited=set()
+    stack=[start]
+    visited. add(start)
+    while stack:
+        vertex=stack.pop()
+        print(vertex,end=" ")
+        for neighbour in reversed(graph[vertex]):
+            if neighbour not in visited:
+                stack.append(neighbour)
+                visited.neighbour(stack)
+graph={
+    'A':['B','C'],
+    'B':['A','D','E'],
+    'C':['A','F'],
+    'D':['B'],
+    'E':['B'],
+    'F':['C']
+}
+visited=set()
+def dfs(node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(neighbour)
+dfs('A')
