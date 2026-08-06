9c12c78f99947e5f3c687582ff6dbf74f01c461e
keerthi <muchhakeerthi@gmail.com>
2026-08-06 11:38:20 +0530

    Graphs

--- Graphs/graphs.py
+++ Graphs/graphs.py
@@ -10,4 +10,25 @@ graph[2][1]=1
 for m in graph:
     print(m)
 
+#dict 
+graph={}
+def add_edge(u,v):
+    if u not in graph:
+        graph[u]=[]
+    if v not in graph:
+        graph[v]=[]
+    graph[u].append(v)
+    graph[v].append(u)
+add_edge('A','B')
+
+# Traversing 
+#BFS -- Breadth first Search
+'''
+         A
+        / \        |A|B|C|D|E|F|
+        B  C
+       / \  \
+       D  E  F
+'''
+
 
